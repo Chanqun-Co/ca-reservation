5fdf0c06a2d27aab2bcdf0da0087c5abe9166b60
KimSeHwan <ksh9241@naver.com>
2024-03-02 10:53:11 +0900

    ca-reservation-1#Eureka 구축 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "3.0.2"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     kotlin("jvm") version "1.7.22"
@@ -37,12 +37,16 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("io.github.microutils:kotlin-logging:1.12.5")
-
         implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-
 
         runtimeOnly("com.mysql:mysql-connector-j")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom ("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
+        }
     }
 
     group = "io.sharing.server"
